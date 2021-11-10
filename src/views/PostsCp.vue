4e66958e664f9af8ddababb2e474caea91403c6f
Sovorong <qhthr417@naver.com>
2021-11-10 16:23:19 +0900

    ...

---
+++ src/views/PostsCp.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="post-wrapper">
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th>번호</th>
+        <th>제목</th>
+        <th>내용</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr v-for=" v in posts" :key="v.id" >
+      <th>{{v.id}}</th>
+        <th>{{v.title}}</th>
+        <th>{{v.body}}</th>
+    </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    const { data } = await axios.get(url)
+    this.posts = data
+    console.log(this.posts)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .post-wrapper {
+   width: 90%;
+   max-width: 720px;
+   margin: 2em auto;
+   overflow-y :auto;
+ }
+</style>
---
+++ src/views/UsersCp.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="users-wrapper">
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th>번호</th>
+        <th>이름</th>
+        <th>전화번호</th>
+        <th>이메일</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr v-for=" v in users" :key="v.id" >
+      <th>{{v.id}}</th>
+        <th>{{v.name}}</th>
+        <th>{{v.phone}}</th>
+        <th>{{v.email}}</th>
+    </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Users',
+  data () {
+    return {
+      users: []
+    }
+  },
+  async created () {
+    const url = 'https://jsonplaceholder.typicode.com/users'
+    const { data } = await axios.get(url)
+    this.users = data
+    console.log(this.users)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .users-wrapper {
+   width: 90%;
+   max-width: 720px;
+   margin: 2em auto;
+   overflow-y :auto;
+ }
+</style>
